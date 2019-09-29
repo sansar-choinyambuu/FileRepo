8e72b831ce0b258d344815fea17d208f071e977f
Sansar Choinyambuu <sansar.choinyambuu@gmail.com>
2019-09-29 08:47:26 +0200

    Newest version

--- det-run.bat
+++ det-run.bat
@@ -1,12 +1,14 @@
 @echo off
 rem Optional argument 1: data file in det subfolder [default: account_data.txt]
-rem Optional argument 1: count of cycles [default 500]
+rem Optional argument 1: count of cycles [default 80]
  
 echo  Data exfiltration toolkit starting ...
  
 rem variables
 set exe="%cd%\det\det.exe"
 set config="%cd%\det\config.json"
+rem with default cycle_total of 80 should run at least 8 hours
+set sleep_seconds=300
  
 if "%~1"=="" (
     set data="%cd%\det\account_data.txt"
@@ -14,7 +16,7 @@ if "%~1"=="" (
     set data="%cd%\det\%~1"
     )
 if "%~2"=="" (
-    set cycle_total=500
+    set cycle_total=80
     ) else (
     set cycle_total=%~2
     )
@@ -44,12 +46,13 @@ call set copied_data=%%data:.=%suffix%%%
 echo f | xcopy /f /y %data% %copied_data%
 set data=%copied_data%
  
-rem replace http proxy username and password placeholders with user entries
-powershell -Command "(Get-Content -encoding UTF8 '%config%').replace('{proxy_user}','%uid%').replace('{proxy_password}','%password%') | Out-File -encoding UTF8 '%config%'"
- 
 rem run data exfiltration with http plugin in loop
 for /l %%x in (1, 1, %cycle_total%) do (
+ 
     @echo off
+    rem replace http proxy username and password placeholders with user entries
+    powershell -Command "(Get-Content -encoding UTF8 '%config%').replace('{proxy_user}','%uid%').replace('{proxy_password}','%password%') | Out-File -encoding UTF8 '%config%'"
+ 
     echo  "|\/\/\/|  "
     echo  "|      |  "
     echo  "|      |  "
@@ -63,10 +66,17 @@ for /l %%x in (1, 1, %cycle_total%) do (
     rem actual det execution
     @echo on
     %exe% -p hypertext-transfer-protocol -c %config% -f %data%
-)
  
-@echo off
-rem replace proxy user and password with placeholders back
-powershell -Command "(Get-Content -encoding UTF8 '%config%').replace('\"proxy_user\": \"%uid%\"','\"proxy_user\": \"{proxy_user}\"').replace('\"proxy_password\": \"%password%\"','\"proxy_password\": \"{proxy_password}\"') | Out-File -encoding UTF8 '%config%'"
+    @echo off
+    rem replace proxy user and password with placeholders back
+    powershell -Command "(Get-Content -encoding UTF8 '%config%').replace('\"proxy_user\": \"%uid%\"','\"proxy_user\": \"{proxy_user}\"').replace('\"proxy_password\": \"%password%\"','\"proxy_password\": \"{proxy_password}\"') | Out-File -encoding UTF8 '%config%'"
+ 
+    if %%x LSS %cycle_total% (
+        rem sleep for given seconds
+        echo *** [Cycle %%x/%cycle_total% done] Sleeping for %sleep_seconds% seconds until next cycle. ***
+        echo *** If you want, you can abort this application with CTRL+C ***
+        TIMEOUT /T %sleep_seconds% /NOBREAK
+    )
+)
  
 echo  Data exfiltration toolkit finishing ...
